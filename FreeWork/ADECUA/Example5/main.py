f967cb2cf13d6449bcb66565c03c99df28270038
ENPEREZ <enrique.perez@siemensgamesa.com>
2020-01-23 17:14:48 +0100

    Fixed transparent rectangle over the image.

--- FreeWork/ADECUA/Example5/main.py
+++ FreeWork/ADECUA/Example5/main.py
@@ -1,38 +1,17 @@
 from tkinter import *
 import cv2
+from PIL import Image, ImageTk
 import numpy as np
-import Image
 
 class Application():
     def __init__(self):
         root = Tk()
         root.title("ADECUA")
 
-        image = cv2.imread('car.png')
-        overlay = image.copy()
-
-        x, y, w, h = 10, 10, 10, 10  # Rectangle parameters
-        cv2.rectangle(overlay, (x, y), (x + w, y + h), (0, 200, 0), -1)  # A filled rectangle
-
-        alpha = 0.4  # Transparency factor.
-
-        # Following line overlays transparent rectangle over the image
-        image_new = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-
-        #cv2.imwrite('car_new.png', image_new)
-        #cv2.imshow("sampple",image_new)
-
-        b, g, r = cv2.split(image_new)
-        image_new2 = cv2.merge((r, g, b))
-
-        # Convert the Image object into a TkPhoto object
-        im = Image.fromarray(image_new2)
-
         # Car image frame
         self.carFrame = Frame(root)
         self.carFrame.grid(row=0, column=0)
-        #self.carPic = PhotoImage(file="./car.png")
-        self.carPic = PhotoImage(image=im)
+        self.carPic = PhotoImage(file="car.png")
         self.carLabel = Label(self.carFrame, image=self.carPic)
         self.carLabel.grid(row=0, column=0)
 
@@ -54,6 +33,27 @@ class Application():
     # print the x,y position of the mouse when the user makes left click
     def leftclick(self, event):
         print('{}, {}'.format(self.x, self.y))
+
+        self.carPicCv2 = cv2.imread('car.png') # load image in open cv format
+        self.carPicCv2Copy = self.carPicCv2.copy() # create a copy of the previous image
+
+        # draw a rectangle oover the co
+        x, y, w, h = 10, 10, 10, 10  # Rectangle parameters
+        cv2.rectangle(self.carPicCv2Copy, (x, y), (x + w, y + h), (0, 200, 0), -1)  # A filled rectangle
+
+        alpha = 0.4  # Transparency factor.
+        self.carPicCv2Mod = cv2.addWeighted(self.carPicCv2Copy, alpha,
+                                            self.carPicCv2, 1 - alpha, 0) # image with the rectangle
+
+        # OpenCV represents images in BGR order; however PIL represents
+        # images in RGB order, so we need to swap the channels
+        b, g, r = cv2.split(self.carPicCv2Mod)
+        self.carPicCV2_RGB = cv2.merge((r, g, b))
+
+        # Convert the Image object into a TkPhoto object
+        self.carPicTk1 = Image.fromarray(self.carPicCV2_RGB)
+        self.carPicTk2 = ImageTk.PhotoImage(image=self.carPicTk1)
+        self.carLabel.configure(image=self.carPicTk2)
 
 
 Application()
