14844c55b4c46956718e03f63e933a8883559c3a
Enrique <ep.delasheras@gmail.com>
2020-02-01 13:50:00 +0100

    Inserted demo items in treeview

--- FreeWork/ADECUA/App/ADECUA.py
+++ FreeWork/ADECUA/App/ADECUA.py
@@ -1,21 +1,23 @@
 from ADECUA_lib import *
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 
 class App():
     def __init__(self):
         #Create and configure the window
         self.root = Tk()
-        self.root.geometry("800x600")
-        self.root.title("ADECUA") #Config window title
-        self.root.iconbitmap(r'.\pics\ADECUA.ico') #Config the icon window
+        self.root.geometry(WIN_SIZE)
+        self.root.title(WIN_TITLE) #Config window title
+        self.root.iconbitmap(WIN_ICO_PATH) #Config the icon window
 
+        # Create main frame to place all the widgets inside
         self.mainWindow = Frame()
         self.mainWindow.grid(row=0, column=0)
         self.mainWindow.columnconfigure(0, weight=1)
         self.mainWindow.rowconfigure(1, weight=1)
 
-        # Create and configure label frames
+        # Create and configure LabelFrames and auto-adjust their sizes
         self.lfTreeview = LabelFrame(self.mainWindow, text="Treeview Selection", labelanchor=N)
         self.lfTreeview.grid(row=0, column=0, sticky="N", padx=20, pady=20)
         self.lfFlatview = LabelFrame(self.mainWindow, text="Flatview", labelanchor=N)
@@ -24,7 +26,6 @@ class App():
         self.lfFloorview.grid(row=0, column=2, sticky="N", padx=20)
         self.lfListbox = LabelFrame(self.mainWindow, text="Listbox", labelanchor=N)
         self.lfListbox.grid(row=0, column=2, sticky="S", padx=40, pady=300)
-
         self.lfTreeview.columnconfigure(0, weight=1)
         self.lfTreeview.rowconfigure(0, weight=1)
         self.lfFlatview.columnconfigure(0, weight=1)
@@ -35,17 +36,22 @@ class App():
         self.lfListbox.rowconfigure(0, weight=1)
 
         # Creating TreeView and bind selected item event
-        self.treeData = createTreeview(self.lfTreeview)
+        self.treeData = ttk.Treeview(self.lfTreeview)
+        self.treeData.heading("#0", text="Treeview")
+        self.treeData.column("#0", minwidth=0, width=200, stretch=NO)
+        addItemsTreeview(self.treeData)
         self.treeData.tag_bind("mytag", "<<TreeviewSelect>>", self.treeItemSelected)
         self.treeData.grid(row=0, column=0, sticky="NEWS")
 
-        # Add default images
+
+        # Create label with flat images
         self.flatPic = Image.open("./pics/flatviews/Dormitorio1_V4.png")
         self.flatPicCopy = self.flatPic.copy()
         self.flatPicTk = ImageTk.PhotoImage(image=self.flatPic)
-        self.lFlatview = Label(self.lfFlatview, image=self.flatPicTk)
-        self.lFlatview.grid(row=0, column=0, sticky="NEWS")
+        self.lFlat = Label(self.lfFlatview, image=self.flatPicTk)
+        self.lFlat.grid(row=0, column=0, sticky="NEWS")
 
+        # Create label with floor image
         self.floorPic = Image.open("./pics/floorviews/PlantaBaja.png")
         self.floorPicTk = ImageTk.PhotoImage(image=self.floorPic)
         self.lFloorview = Label(self.lfFloorview, image=self.floorPicTk)
@@ -58,16 +64,16 @@ class App():
         self.yscroll.grid(row=0,column=1, sticky="NS")
         self.listData.config(yscrollcommand=self.yscroll.set) # Enable yscroll on the listbox
         self.yscroll.config(command=self.listData.yview)
-        addItemListbox(self.listData)
+        addItemsListbox(self.listData)
         self.listData.bind("<<ListboxSelect>>",lambda event: self.clickOptionlist(event))
 
-        self.root.bind("<Configure>", self.resize_handler)  # bind the function to resize window
-
+        self.root.bind("<Configure>", self.resize_handler)  # bind to windows sizing event
         self.root.mainloop()
 
     def treeItemSelected(self, event):
         """Item from treeData selected."""
         selected_items = self.treeData.selection()
+        #print(self.treeData.item(selected_items)["text"])
         print(self.treeData.item(selected_items)["text"])
 
     # method related to the item selected on the listData
@@ -76,12 +82,12 @@ class App():
         selection = widget.curselection()
         value = widget.get(selection[0])  # get the string from the item selected on the listbox
         print(value)
-        if str(value) == "Subelemento 1":
-            self.treeData.item("Main", open=True)
-            self.treeData.selection_set("Sub1")
-        elif str(value) == "Subelemento 2":
-            self.treeData.item("Main", open=True)
-            self.treeData.selection_set("Sub2")
+        if str(value) == TL_FLAT_VAR[0]:
+            self.treeData.item(TL_FLAT_VAR[0], open=True)
+            self.treeData.selection_set(TL_FLAT_VAR[0])
+        elif str(value) == TL_FLAT_VAR[1]:
+            self.treeData.item(TL_FLAT_VAR[1], open=True)
+            self.treeData.selection_set(TL_FLAT_VAR[1])
 
     def resize_handler(self, event):
         # determine the ratio of old width/height to new width/height
@@ -93,22 +99,22 @@ class App():
 
         if self.root.state() == "zoomed":
             if hWindow > hEvent:
-                wPicSize = 800
-                hPicSize = 300
-                print("Window size es: {}x{}".format(wWindow, hWindow))
-                print("Event size es: {}x{}".format(wEvent, hEvent))
-                print("Pic size es: {}x{}".format(wPicSize, hPicSize))
+                wPicSize = 600
+                hPicSize = 400
+                #print("Window size es: {}x{}".format(wWindow, hWindow))
+                #print("Event size es: {}x{}".format(wEvent, hEvent))
+                #print("Pic size es: {}x{}".format(wPicSize, hPicSize))
                 self.flatPic = self.flatPicCopy.resize((wPicSize, hPicSize), Image.ANTIALIAS)
                 self.flatPicTk = ImageTk.PhotoImage(image=self.flatPic)
-                self.lFlatview.configure(image=self.flatPicTk)
+                self.lFlat.configure(image=self.flatPicTk)
         else:
             wPicSize = 400
             hPicSize = 300
-            print("Window size es: {}x{}".format(wWindow, hWindow))
-            print("Event size es: {}x{}".format(wEvent, hEvent))
-            print("Pic size es: {}x{}".format(wPicSize, hPicSize))
+            #print("Window size es: {}x{}".format(wWindow, hWindow))
+            #print("Event size es: {}x{}".format(wEvent, hEvent))
+            #print("Pic size es: {}x{}".format(wPicSize, hPicSize))
             self.flatPic = self.flatPicCopy.resize((wPicSize, hPicSize), Image.ANTIALIAS)
             self.flatPicTk = ImageTk.PhotoImage(image=self.flatPic)
-            self.lFlatview.configure(image=self.flatPicTk)
+            self.lFlat.configure(image=self.flatPicTk)
 
 App()
--- FreeWork/ADECUA/App/ADECUA_lib.py
+++ FreeWork/ADECUA/App/ADECUA_lib.py
@@ -1,24 +1,182 @@
 from tkinter import *
-from tkinter import ttk
 
-def createTreeview(Frame):
-    treeview = ttk.Treeview(Frame)
+# root window
+WIN_SIZE = "800x600"
+WIN_TITLE = "ADECUA"
+WIN_ICO_PATH = ".\pics\ADECUA.ico"
 
+# Treeview + Listbox
+TL_BLK_IID = ["blk1", "blk2", "blk3"]
+TL_FLOOR_IID = ["blk1_floor_0", "blk1_floor_1", "blk1_floor_2", "blk1_floor_3",
+                "blk2_floor_0", "blk2_floor_1", "blk2_floor_2", "blk2_floor_3",
+                "blk3_floor_0", "blk3_floor_1", "blk3_floor_2", "blk3_floor_3"
+               ]
+TL_FLAT_IID = ["blk1_floor0_flat_1r", "blk1_floor0_flat_2r", "blk1_floor0_flat_3r",
+               "blk1_floor1_flat_1r", "blk1_floor1_flat_2r", "blk1_floor1_flat_3r",
+               "blk1_floor2_flat_1r", "blk1_floor2_flat_2r", "blk1_floor2_flat_3r",
+               "blk1_floor3_flat_1r", "blk1_floor3_flat_2r", "blk1_floor3_flat_3r",
+
+               "blk2_floor0_flat_1r", "blk2_floor0_flat_2r", "blk2_floor0_flat_3r",
+               "blk2_floor1_flat_1r", "blk2_floor1_flat_2r", "blk2_floor1_flat_3r",
+               "blk2_floor2_flat_1r", "blk2_floor2_flat_2r", "blk2_floor2_flat_3r",
+               "blk2_floor3_flat_1r", "blk2_floor3_flat_2r", "blk2_floor3_flat_3r",
+
+               "blk3_floor0_flat_1r", "blk3_floor0_flat_2r", "blk3_floor0_flat_3r",
+               "blk3_floor1_flat_1r", "blk3_floor1_flat_2r", "blk3_floor1_flat_3r",
+               "blk3_floor2_flat_1r", "blk3_floor2_flat_2r", "blk3_floor2_flat_3r",
+               "blk3_floor3_flat_1r", "blk3_floor3_flat_2r", "blk3_floor3_flat_3r",
+               ]
+TL_FLAT_VAR_IID = ["blk1_floor0_flat1r_1DVar1", "blk1_floor0_flat1r_1DVar2", "blk1_floor0_flat1r_1DVar3",
+                   "blk1_floor1_flat1r_1DVar1", "blk1_floor1_flat1r_1DVar2", "blk1_floor1_flat1r_1DVar3",
+                   "blk1_floor2_flat1r_1DVar1", "blk1_floor2_flat1r_1DVar2", "blk1_floor2_flat1r_1DVar3",
+                   "blk1_floor3_flat1r_1DVar1", "blk1_floor3_flat1r_1DVar2", "blk1_floor3_flat1r_1DVar3",
+			       "blk1_floor0_flat2r_1DVar1", "blk1_floor0_flat2r_1DVar2", "blk1_floor0_flat2r_1DVar3",
+                   "blk1_floor1_flat2r_1DVar1", "blk1_floor1_flat2r_1DVar2", "blk1_floor1_flat2r_1DVar3",
+                   "blk1_floor2_flat2r_1DVar1", "blk1_floor2_flat2r_1DVar2", "blk1_floor2_flat2r_1DVar3",
+                   "blk1_floor3_flat2r_1DVar1", "blk1_floor3_flat2r_1DVar2", "blk1_floor3_flat2r_1DVar3",
+		  	       "blk1_floor0_flat3r_1DVar1", "blk1_floor0_flat3r_1DVar2", "blk1_floor0_flat3r_1DVar3",
+                   "blk1_floor1_flat3r_1DVar1", "blk1_floor1_flat3r_1DVar2", "blk1_floor1_flat3r_1DVar3",
+                   "blk1_floor2_flat3r_1DVar1", "blk1_floor2_flat3r_1DVar2", "blk1_floor2_flat3r_1DVar3",
+                   "blk1_floor3_flat3r_1DVar1", "blk1_floor3_flat3r_1DVar2", "blk1_floor3_flat3r_1DVar3",
+                   "blk2_floor0_flat1r_1DVar1", "blk2_floor0_flat1r_1DVar2", "blk2_floor0_flat1r_1DVar3",
+                   "blk2_floor1_flat1r_1DVar1", "blk2_floor1_flat1r_1DVar2", "blk2_floor1_flat1r_1DVar3",
+                   "blk2_floor2_flat1r_1DVar1", "blk2_floor2_flat1r_1DVar2", "blk2_floor2_flat1r_1DVar3",
+                   "blk2_floor3_flat1r_1DVar1", "blk2_floor3_flat1r_1DVar2", "blk2_floor3_flat1r_1DVar3",
+			       "blk2_floor0_flat2r_1DVar1", "blk2_floor0_flat2r_1DVar2", "blk2_floor0_flat2r_1DVar3",
+                   "blk2_floor1_flat2r_1DVar1", "blk2_floor1_flat2r_1DVar2", "blk2_floor1_flat2r_1DVar3",
+                   "blk2_floor2_flat2r_1DVar1", "blk2_floor2_flat2r_1DVar2", "blk2_floor2_flat2r_1DVar3",
+                   "blk2_floor3_flat2r_1DVar1", "blk2_floor3_flat2r_1DVar2", "blk2_floor3_flat2r_1DVar3",
+			       "blk2_floor0_flat3r_1DVar1", "blk2_floor0_flat3r_1DVar2", "blk2_floor0_flat3r_1DVar3",
+                   "blk2_floor1_flat3r_1DVar1", "blk2_floor1_flat3r_1DVar2", "blk2_floor1_flat3r_1DVar3",
+                   "blk2_floor2_flat3r_1DVar1", "blk2_floor2_flat3r_1DVar2", "blk2_floor2_flat3r_1DVar3",
+                   "blk2_floor3_flat3r_1DVar1", "blk2_floor3_flat3r_1DVar2", "blk2_floor3_flat3r_1DVar3",
+                   "blk3_floor0_flat1r_1DVar1", "blk3_floor0_flat1r_1DVar2", "blk3_floor0_flat1r_1DVar3",
+                   "blk3_floor1_flat1r_1DVar1", "blk3_floor1_flat1r_1DVar2", "blk3_floor1_flat1r_1DVar3",
+                   "blk3_floor2_flat1r_1DVar1", "blk3_floor2_flat1r_1DVar2", "blk3_floor2_flat1r_1DVar3",
+                   "blk3_floor3_flat1r_1DVar1", "blk3_floor3_flat1r_1DVar2", "blk3_floor3_flat1r_1DVar3",
+			       "blk3_floor0_flat2r_1DVar1", "blk3_floor0_flat2r_1DVar2", "blk3_floor0_flat2r_1DVar3",
+                   "blk3_floor1_flat2r_1DVar1", "blk3_floor1_flat2r_1DVar2", "blk3_floor1_flat2r_1DVar3",
+                   "blk3_floor2_flat2r_1DVar1", "blk3_floor2_flat2r_1DVar2", "blk3_floor2_flat2r_1DVar3",
+                   "blk3_floor3_flat2r_1DVar1", "blk3_floor3_flat2r_1DVar2", "blk3_floor3_flat2r_1DVar3",
+			       "blk3_floor0_flat3r_1DVar1", "blk3_floor0_flat3r_1DVar2", "blk3_floor0_flat3r_1DVar3",
+                   "blk3_floor1_flat3r_1DVar1", "blk3_floor1_flat3r_1DVar2", "blk3_floor1_flat3r_1DVar3",
+                   "blk3_floor2_flat3r_1DVar1", "blk3_floor2_flat3r_1DVar2", "blk3_floor2_flat3r_1DVar3",
+                   "blk3_floor3_flat3r_1DVar1", "blk3_floor3_flat3r_1DVar2", "blk3_floor3_flat3r_1DVar3",
+
+				   "blk1_floor0_flat1r_2DVar1", "blk1_floor0_flat1r_2DVar2", "blk1_floor0_flat1r_2DVar3",
+                   "blk1_floor1_flat1r_2DVar1", "blk1_floor1_flat1r_2DVar2", "blk1_floor1_flat1r_2DVar3",
+                   "blk1_floor2_flat1r_2DVar1", "blk1_floor2_flat1r_2DVar2", "blk1_floor2_flat1r_2DVar3",
+                   "blk1_floor3_flat1r_2DVar1", "blk1_floor3_flat1r_2DVar2", "blk1_floor3_flat1r_2DVar3",
+			       "blk1_floor0_flat2r_2DVar1", "blk1_floor0_flat2r_2DVar2", "blk1_floor0_flat2r_2DVar3",
+                   "blk1_floor1_flat2r_2DVar1", "blk1_floor1_flat2r_2DVar2", "blk1_floor1_flat2r_2DVar3",
+                   "blk1_floor2_flat2r_2DVar1", "blk1_floor2_flat2r_2DVar2", "blk1_floor2_flat2r_2DVar3",
+                   "blk1_floor3_flat2r_2DVar1", "blk1_floor3_flat2r_2DVar2", "blk1_floor3_flat2r_2DVar3",
+		  	       "blk1_floor0_flat3r_2DVar1", "blk1_floor0_flat3r_2DVar2", "blk1_floor0_flat3r_2DVar3",
+                   "blk1_floor1_flat3r_2DVar1", "blk1_floor1_flat3r_2DVar2", "blk1_floor1_flat3r_2DVar3",
+                   "blk1_floor2_flat3r_2DVar1", "blk1_floor2_flat3r_2DVar2", "blk1_floor2_flat3r_2DVar3",
+                   "blk1_floor3_flat3r_2DVar1", "blk1_floor3_flat3r_2DVar2", "blk1_floor3_flat3r_2DVar3",
+                   "blk2_floor0_flat1r_2DVar1", "blk2_floor0_flat1r_2DVar2", "blk2_floor0_flat1r_2DVar3",
+                   "blk2_floor1_flat1r_2DVar1", "blk2_floor1_flat1r_2DVar2", "blk2_floor1_flat1r_2DVar3",
+                   "blk2_floor2_flat1r_2DVar1", "blk2_floor2_flat1r_2DVar2", "blk2_floor2_flat1r_2DVar3",
+                   "blk2_floor3_flat1r_2DVar1", "blk2_floor3_flat1r_2DVar2", "blk2_floor3_flat1r_2DVar3",
+			       "blk2_floor0_flat2r_2DVar1", "blk2_floor0_flat2r_2DVar2", "blk2_floor0_flat2r_2DVar3",
+                   "blk2_floor1_flat2r_2DVar1", "blk2_floor1_flat2r_2DVar2", "blk2_floor1_flat2r_2DVar3",
+                   "blk2_floor2_flat2r_2DVar1", "blk2_floor2_flat2r_2DVar2", "blk2_floor2_flat2r_2DVar3",
+                   "blk2_floor3_flat2r_2DVar1", "blk2_floor3_flat2r_2DVar2", "blk2_floor3_flat2r_2DVar3",
+			       "blk2_floor0_flat3r_2DVar1", "blk2_floor0_flat3r_2DVar2", "blk2_floor0_flat3r_2DVar3",
+                   "blk2_floor1_flat3r_2DVar1", "blk2_floor1_flat3r_2DVar2", "blk2_floor1_flat3r_2DVar3",
+                   "blk2_floor2_flat3r_2DVar1", "blk2_floor2_flat3r_2DVar2", "blk2_floor2_flat3r_2DVar3",
+                   "blk2_floor3_flat3r_2DVar1", "blk2_floor3_flat3r_2DVar2", "blk2_floor3_flat3r_2DVar3",
+                   "blk3_floor0_flat1r_2DVar1", "blk3_floor0_flat1r_2DVar2", "blk3_floor0_flat1r_2DVar3",
+                   "blk3_floor1_flat1r_2DVar1", "blk3_floor1_flat1r_2DVar2", "blk3_floor1_flat1r_2DVar3",
+                   "blk3_floor2_flat1r_2DVar1", "blk3_floor2_flat1r_2DVar2", "blk3_floor2_flat1r_2DVar3",
+                   "blk3_floor3_flat1r_2DVar1", "blk3_floor3_flat1r_2DVar2", "blk3_floor3_flat1r_2DVar3",
+			       "blk3_floor0_flat2r_2DVar1", "blk3_floor0_flat2r_2DVar2", "blk3_floor0_flat2r_2DVar3",
+                   "blk3_floor1_flat2r_2DVar1", "blk3_floor1_flat2r_2DVar2", "blk3_floor1_flat2r_2DVar3",
+                   "blk3_floor2_flat2r_2DVar1", "blk3_floor2_flat2r_2DVar2", "blk3_floor2_flat2r_2DVar3",
+                   "blk3_floor3_flat2r_2DVar1", "blk3_floor3_flat2r_2DVar2", "blk3_floor3_flat2r_2DVar3",
+			       "blk3_floor0_flat3r_2DVar1", "blk3_floor0_flat3r_2DVar2", "blk3_floor0_flat3r_2DVar3",
+                   "blk3_floor1_flat3r_2DVar1", "blk3_floor1_flat3r_2DVar2", "blk3_floor1_flat3r_2DVar3",
+                   "blk3_floor2_flat3r_2DVar1", "blk3_floor2_flat3r_2DVar2", "blk3_floor2_flat3r_2DVar3",
+                   "blk3_floor3_flat3r_2DVar1", "blk3_floor3_flat3r_2DVar2", "blk3_floor3_flat3r_2DVar3",
+
+				   "blk1_floor0_flat1r_3DVar1", "blk1_floor0_flat1r_3DVar2", "blk1_floor0_flat1r_3DVar3",
+                   "blk1_floor1_flat1r_3DVar1", "blk1_floor1_flat1r_3DVar2", "blk1_floor1_flat1r_3DVar3",
+                   "blk1_floor2_flat1r_3DVar1", "blk1_floor2_flat1r_3DVar2", "blk1_floor2_flat1r_3DVar3",
+                   "blk1_floor3_flat1r_3DVar1", "blk1_floor3_flat1r_3DVar2", "blk1_floor3_flat1r_3DVar3",
+			       "blk1_floor0_flat2r_3DVar1", "blk1_floor0_flat2r_3DVar2", "blk1_floor0_flat2r_3DVar3",
+                   "blk1_floor1_flat2r_3DVar1", "blk1_floor1_flat2r_3DVar2", "blk1_floor1_flat2r_3DVar3",
+                   "blk1_floor2_flat2r_3DVar1", "blk1_floor2_flat2r_3DVar2", "blk1_floor2_flat2r_3DVar3",
+                   "blk1_floor3_flat2r_3DVar1", "blk1_floor3_flat2r_3DVar2", "blk1_floor3_flat2r_3DVar3",
+		  	       "blk1_floor0_flat3r_3DVar1", "blk1_floor0_flat3r_3DVar2", "blk1_floor0_flat3r_3DVar3",
+                   "blk1_floor1_flat3r_3DVar1", "blk1_floor1_flat3r_3DVar2", "blk1_floor1_flat3r_3DVar3",
+                   "blk1_floor2_flat3r_3DVar1", "blk1_floor2_flat3r_3DVar2", "blk1_floor2_flat3r_3DVar3",
+                   "blk1_floor3_flat3r_3DVar1", "blk1_floor3_flat3r_3DVar2", "blk1_floor3_flat3r_3DVar3",
+                   "blk2_floor0_flat1r_3DVar1", "blk2_floor0_flat1r_3DVar2", "blk2_floor0_flat1r_3DVar3",
+                   "blk2_floor1_flat1r_3DVar1", "blk2_floor1_flat1r_3DVar2", "blk2_floor1_flat1r_3DVar3",
+                   "blk2_floor2_flat1r_3DVar1", "blk2_floor2_flat1r_3DVar2", "blk2_floor2_flat1r_3DVar3",
+                   "blk2_floor3_flat1r_3DVar1", "blk2_floor3_flat1r_3DVar2", "blk2_floor3_flat1r_3DVar3",
+			       "blk2_floor0_flat2r_3DVar1", "blk2_floor0_flat2r_3DVar2", "blk2_floor0_flat2r_3DVar3",
+                   "blk2_floor1_flat2r_3DVar1", "blk2_floor1_flat2r_3DVar2", "blk2_floor1_flat2r_3DVar3",
+                   "blk2_floor2_flat2r_3DVar1", "blk2_floor2_flat2r_3DVar2", "blk2_floor2_flat2r_3DVar3",
+                   "blk2_floor3_flat2r_3DVar1", "blk2_floor3_flat2r_3DVar2", "blk2_floor3_flat2r_3DVar3",
+			       "blk2_floor0_flat3r_3DVar1", "blk2_floor0_flat3r_3DVar2", "blk2_floor0_flat3r_3DVar3",
+                   "blk2_floor1_flat3r_3DVar1", "blk2_floor1_flat3r_3DVar2", "blk2_floor1_flat3r_3DVar3",
+                   "blk2_floor2_flat3r_3DVar1", "blk2_floor2_flat3r_3DVar2", "blk2_floor2_flat3r_3DVar3",
+                   "blk2_floor3_flat3r_3DVar1", "blk2_floor3_flat3r_3DVar2", "blk2_floor3_flat3r_3DVar3",
+                   "blk3_floor0_flat1r_3DVar1", "blk3_floor0_flat1r_3DVar2", "blk3_floor0_flat1r_3DVar3",
+                   "blk3_floor1_flat1r_3DVar1", "blk3_floor1_flat1r_3DVar2", "blk3_floor1_flat1r_3DVar3",
+                   "blk3_floor2_flat1r_3DVar1", "blk3_floor2_flat1r_3DVar2", "blk3_floor2_flat1r_3DVar3",
+                   "blk3_floor3_flat1r_3DVar1", "blk3_floor3_flat1r_3DVar2", "blk3_floor3_flat1r_3DVar3",
+			       "blk3_floor0_flat2r_3DVar1", "blk3_floor0_flat2r_3DVar2", "blk3_floor0_flat2r_3DVar3",
+                   "blk3_floor1_flat2r_3DVar1", "blk3_floor1_flat2r_3DVar2", "blk3_floor1_flat2r_3DVar3",
+                   "blk3_floor2_flat2r_3DVar1", "blk3_floor2_flat2r_3DVar2", "blk3_floor2_flat2r_3DVar3",
+                   "blk3_floor3_flat2r_3DVar1", "blk3_floor3_flat2r_3DVar2", "blk3_floor3_flat2r_3DVar3",
+			       "blk3_floor0_flat3r_3DVar1", "blk3_floor0_flat3r_3DVar2", "blk3_floor0_flat3r_3DVar3",
+                   "blk3_floor1_flat3r_3DVar1", "blk3_floor1_flat3r_3DVar2", "blk3_floor1_flat3r_3DVar3",
+                   "blk3_floor2_flat3r_3DVar1", "blk3_floor2_flat3r_3DVar2", "blk3_floor2_flat3r_3DVar3",
+                   "blk3_floor3_flat3r_3DVar1", "blk3_floor3_flat3r_3DVar2", "blk3_floor3_flat3r_3DVar3"]
+
+TL_BLOCK = ["Bloque 1", "Bloque 2", "Bloque 3"]
+TL_FLOOR = ["Planta Baja", "Planta 1ª", "Planta 2ª", "Planta 3ª"]
+TL_FLAT = ["1 Dormitorio", "2 Dormitorios", "3 Dormitorios"]
+TL_FLAT_VAR = ["1DVar1", "1DVar2", "1DVar3",
+                "2DVar1", "2DVar2", "2DVar3",
+                "3DVar1", "3DVar2", "3DVar3"]
+
+def addItemsTreeview(Treeview):
     # adding elements to the tree. The previous tree label is indicated
     # to have an event response
-    treeview.insert("", END, text="Elemento 1", iid="Main", tags=("mytag",))
-    treeview.insert("Main", END, text="Subelemento 1", iid="Sub1", tags=("mytag",))
-    treeview.insert("Main", END, text="Subelemento 2", iid="Sub2", tags=("mytag",))
 
-    return treeview
+    #*************BLOCK1****************
+    for i in range(len(TL_BLOCK)):
+        Treeview.insert("", END, text=TL_BLOCK[i], iid=TL_BLK_IID[i], tags=("mytag",))
+    # Floors
+    k = 0
+    for i in range(len(TL_BLOCK)):
+        for j in range(len(TL_FLOOR)):
+            #print("i={}, j={}, k={}".format(i, j, k))
+            Treeview.insert(TL_BLK_IID[i], END, text=TL_FLOOR[j], iid=TL_FLOOR_IID[k], tags=("mytag",))
+            k += 1
+    # Rooms
+    k = 0
+    for i in range(len(TL_FLOOR_IID)):
+        for j in range(len(TL_FLAT)):
+            #print("i={}, j={}, k={}".format(i, j, k))
+            Treeview.insert(TL_FLOOR_IID[i], END, text=TL_FLAT[j], iid=TL_FLAT_IID[k], tags=("mytag",))
+            k += 1
 
-def addItemListbox(ListBox):
+    # Roms variants
+    k = 0
+    print(len(TL_FLAT_VAR_IID))
+    for i in range(len(TL_FLAT_IID)):
+        for j in range(len(TL_FLAT_VAR)):
+            print("i={}, j={}, k={}".format(i, j, k))
+            Treeview.insert(TL_FLAT_IID[i], END, text=TL_FLAT_VAR[j], iid=TL_FLAT_VAR_IID[k], tags=("mytag",))
+            k += 1
 
-    ListBox.insert(0, "Subelemento 1")
-    ListBox.insert(1, "Subelemento 2")
-    ListBox.insert(2, "Subelemento 3")
-    ListBox.insert(3, "Subelemento 4")
-    ListBox.insert(4, "Subelemento 5")
-    ListBox.insert(5, "Subelemento 6")
+def addItemsListbox(ListBox):
 
-    #return listbox
+    ListBox.insert(0, "1DVar1")
+    ListBox.insert(1, "1DVar1")
+    ListBox.insert(2, "1DVar1")
