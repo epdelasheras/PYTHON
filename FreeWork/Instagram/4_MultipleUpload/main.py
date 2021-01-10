fb252dfd391bc41111df9e6dcd9b77bb9d059a24
Enrique <ep.delasheras@gmail.com>
2021-01-10 10:05:31 +0100

    selenium + instagram => Multiple photo upload does not achieved

--- FreeWork/Instagram/4_MultipleUpload/main.py
+++ FreeWork/Instagram/4_MultipleUpload/main.py
@@ -1,19 +1,16 @@
+import autoit # pip install -U pyautoit
 import os
 import time
-#from my_lib import *
+from my_lib import *
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 
 site = "https://www.instagram.com/accounts/login/"
 
-driver = webdriver.Chrome("chromedriver.exe")
+driver = webdriver.Chrome("chromedriver.exe", options=get_options())
 driver.get(site)
 
 # wait some time until load page
-time.sleep(1)
+time.sleep(3)
 
 # Press button to accept cookies
 driver.find_element_by_xpath('/html/body/div[2]/div/div/div/div[2]/button[1]').click()
@@ -28,27 +25,44 @@ passw = driver.find_element_by_name('password')
 passw.send_keys('kikazo')
 
 # Press login button
-driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div[1]/div/form/div/div[3]/button/div').click()
+driver.find_element_by_xpath('/html/body/div[1]/section/main/div[1]/div/div/div/form/div[1]/div[6]/button').click()
 
-time.sleep(5)
+time.sleep(3)
 
-# Press "Ahora no" button for not saving session info
-driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/div/button').click()
+# Press "Ahora no" button for not saving info
+driver.find_element_by_xpath('/html/body/div[1]/section/main/div/div/div/button').click()
 
-time.sleep(1)
+time.sleep(2)
 
-# Press "Ahora no" for not enabling notifications
+# Press "Cancelar" button for not adding info to the starting screen
 driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]').click()
 
-'''
-user.send_keys('portatest')
+time.sleep(2)
 
-passw = driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input')
-passw.send_keys('kikazo')
+picname = "PortadaMarca"
+image_resize(picname)
 
-driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[3]').click()
+# 1. Click upload image button
+print('Clicking `upload` button...')
+driver.find_element_by_xpath("//div[@role='menuitem']").click()
+time.sleep(2)
 
-#picname = "PortadaMarca"
-#image_resize(picname)
-'''
-print("fin")
+# 2. Get the windows file explorer window that opened
+# Add the image path and click enter
+print('Uploading image to file explorer...')
+autoit.win_active("Open")
+file_path = 'PortadaMarca_resize.jpg'
+autoit.control_send("Abrir", "Edit1", os.path.normpath(os.getcwd() + '/' + file_path))
+autoit.control_send("Abrir", "Edit1", "{ENTER}")
+time.sleep(2)
+
+# 3. Image should import and click Next button
+print('Image importing and processed to description...')
+driver.find_element_by_xpath('//*[@id="react-root"]/section/div[1]/header/div/div[2]/button').click()
+time.sleep(2)
+
+# 5. CLick Share
+print('Clicking `Share`!')
+driver.find_element_by_xpath('//*[@id="react-root"]/section/div[1]/header/div/div[2]/button').click()
+time.sleep(4)
+print('Upload completed.')
--- FreeWork/Instagram/4_MultipleUpload/my_lib.py
+++ FreeWork/Instagram/4_MultipleUpload/my_lib.py
@@ -1,6 +1,7 @@
 
 from PIL import Image
 import math
+from selenium.webdriver.chrome.options import *
 
 def make_square(im, min_size=1080, fill_color=(255, 255, 255)):
     #here getting the width and the height of the picture and after that adjusting it's size for it to fit on a 1080px picture perfectly
@@ -25,3 +26,15 @@ def image_resize (picname):
     img_pil = Image.open(picname + ".jpg")
     img_resize = make_square(img_pil, 1080, (255, 255, 255))
     img_resize = img_resize.save(picname + "_resize.jpg")
+
+def get_options():
+    options = Options()
+    options.add_argument("--log-level=3")
+    options.add_argument("--silent")
+    # options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-logging")
+    options.add_argument("--mute-audio")
+    #options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+    options.add_argument('--user-agent=Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Mobile Safari/537.36')
+    return options
