dec1485461adbd10ff3eb7614456481df3445b1b
epdelasheras <ep.delasheras@gmail.com>
2021-01-23 21:45:40 +0100

    adapted to the new gui with save button

--- FreeWork/Instagram/3_GUI/main.py
+++ FreeWork/Instagram/3_GUI/main.py
@@ -22,7 +22,7 @@ class Ui_MainWindow(object):
         self.line_url_as.setGeometry(QtCore.QRect(100, 70, 331, 20))
         self.line_url_as.setObjectName("line_url_as")
         self.label_step2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_step2.setGeometry(QtCore.QRect(20, 780, 241, 16))
+        self.label_step2.setGeometry(QtCore.QRect(20, 780, 341, 16))
         self.label_step2.setObjectName("label_step2")
         self.label_step1 = QtWidgets.QLabel(self.centralwidget)
         self.label_step1.setGeometry(QtCore.QRect(20, 30, 121, 16))
@@ -31,10 +31,10 @@ class Ui_MainWindow(object):
         self.label_url_as.setGeometry(QtCore.QRect(50, 70, 31, 16))
         self.label_url_as.setObjectName("label_url_as")
         self.label_url_marca = QtWidgets.QLabel(self.centralwidget)
-        self.label_url_marca.setGeometry(QtCore.QRect(450, 70, 31, 16))
+        self.label_url_marca.setGeometry(QtCore.QRect(520, 70, 31, 16))
         self.label_url_marca.setObjectName("label_url_marca")
         self.label_url_mundo = QtWidgets.QLabel(self.centralwidget)
-        self.label_url_mundo.setGeometry(QtCore.QRect(430, 430, 91, 16))
+        self.label_url_mundo.setGeometry(QtCore.QRect(480, 430, 91, 16))
         self.label_url_mundo.setObjectName("label_url_mundo")
         self.label_url_sport = QtWidgets.QLabel(self.centralwidget)
         self.label_url_sport.setGeometry(QtCore.QRect(50, 430, 41, 16))
@@ -54,7 +54,7 @@ class Ui_MainWindow(object):
         self.label_pic_as.setAlignment(QtCore.Qt.AlignCenter)
         self.label_pic_as.setObjectName("label_pic_as")
         self.label_pic_marca = QtWidgets.QLabel(self.centralwidget)
-        self.label_pic_marca.setGeometry(QtCore.QRect(540, 100, 251, 291))
+        self.label_pic_marca.setGeometry(QtCore.QRect(620, 100, 251, 291))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -68,7 +68,7 @@ class Ui_MainWindow(object):
         self.label_pic_marca.setAlignment(QtCore.Qt.AlignCenter)
         self.label_pic_marca.setObjectName("label_pic_marca")
         self.line_url_marca = QtWidgets.QLineEdit(self.centralwidget)
-        self.line_url_marca.setGeometry(QtCore.QRect(500, 70, 331, 20))
+        self.line_url_marca.setGeometry(QtCore.QRect(560, 70, 331, 20))
         self.line_url_marca.setObjectName("line_url_marca")
         self.label_pic_sport = QtWidgets.QLabel(self.centralwidget)
         self.label_pic_sport.setGeometry(QtCore.QRect(140, 460, 251, 291))
@@ -85,7 +85,7 @@ class Ui_MainWindow(object):
         self.label_pic_sport.setAlignment(QtCore.Qt.AlignCenter)
         self.label_pic_sport.setObjectName("label_pic_sport")
         self.label_pic_mundo = QtWidgets.QLabel(self.centralwidget)
-        self.label_pic_mundo.setGeometry(QtCore.QRect(540, 460, 251, 291))
+        self.label_pic_mundo.setGeometry(QtCore.QRect(630, 460, 251, 291))
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -102,7 +102,7 @@ class Ui_MainWindow(object):
         self.line_url_sport.setGeometry(QtCore.QRect(90, 430, 331, 20))
         self.line_url_sport.setObjectName("line_url_sport")
         self.line_url_mundo = QtWidgets.QLineEdit(self.centralwidget)
-        self.line_url_mundo.setGeometry(QtCore.QRect(520, 430, 331, 20))
+        self.line_url_mundo.setGeometry(QtCore.QRect(580, 430, 331, 20))
         self.line_url_mundo.setObjectName("line_url_mundo")
         self.label_insta_user = QtWidgets.QLabel(self.centralwidget)
         self.label_insta_user.setGeometry(QtCore.QRect(20, 840, 31, 16))
@@ -119,15 +119,12 @@ class Ui_MainWindow(object):
         self.pushButton_GetPics = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_GetPics.setGeometry(QtCore.QRect(170, 30, 75, 23))
         self.pushButton_GetPics.setObjectName("pushButton_GetPics")
-        self.pushButton_Login = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_Login.setGeometry(QtCore.QRect(220, 860, 75, 23))
-        self.pushButton_Login.setObjectName("pushButton_Login")
-        self.label_step3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_step3.setGeometry(QtCore.QRect(370, 780, 111, 16))
-        self.label_step3.setObjectName("label_step3")
-        self.pushButton_Upload = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_Upload.setGeometry(QtCore.QRect(510, 780, 75, 23))
-        self.pushButton_Upload.setObjectName("pushButton_Upload")
+        self.pushButton_LoginUpload = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_LoginUpload.setGeometry(QtCore.QRect(390, 780, 101, 23))
+        self.pushButton_LoginUpload.setObjectName("pushButton_LoginUpload")
+        self.pushButton_Save = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_Save.setGeometry(QtCore.QRect(90, 900, 75, 23))
+        self.pushButton_Save.setObjectName("pushButton_Save")
         self.label_Title = QtWidgets.QLabel(self.centralwidget)
         self.label_Title.setGeometry(QtCore.QRect(310, 10, 511, 31))
         self.label_Title.setFrameShape(QtWidgets.QFrame.Box)
@@ -139,16 +136,16 @@ class Ui_MainWindow(object):
         self.label_Author.setGeometry(QtCore.QRect(800, 920, 191, 20))
         self.label_Author.setObjectName("label_Author")
         self.label_post = QtWidgets.QLabel(self.centralwidget)
-        self.label_post.setGeometry(QtCore.QRect(470, 810, 41, 16))
+        self.label_post.setGeometry(QtCore.QRect(240, 850, 41, 16))
         self.label_post.setObjectName("label_post")
         self.label_hashtag = QtWidgets.QLabel(self.centralwidget)
-        self.label_hashtag.setGeometry(QtCore.QRect(760, 780, 81, 16))
+        self.label_hashtag.setGeometry(QtCore.QRect(570, 850, 81, 16))
         self.label_hashtag.setObjectName("label_hashtag")
         self.text_post = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_post.setGeometry(QtCore.QRect(360, 830, 271, 71))
+        self.text_post.setGeometry(QtCore.QRect(280, 810, 271, 91))
         self.text_post.setObjectName("text_post")
         self.text_hashtag = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_hashtag.setGeometry(QtCore.QRect(640, 800, 341, 111))
+        self.text_hashtag.setGeometry(QtCore.QRect(640, 800, 341, 101))
         self.text_hashtag.setObjectName("text_hashtag")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -161,7 +158,7 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_step2.setText(_translate("MainWindow", "STEP2: LOGIN INTO YOUR INSTAGRAM ACCOUNT"))
+        self.label_step2.setText(_translate("MainWindow", "STEP2: LOGIN INTO YOUR INSTAGRAM ACCOUNT AND UPLOAD PICS"))
         self.label_step1.setText(_translate("MainWindow", "STEP 1: GET PIC LINKS"))
         self.label_url_as.setText(_translate("MainWindow", "As"))
         self.label_url_marca.setText(_translate("MainWindow", "Marca"))
@@ -170,39 +167,53 @@ class Ui_MainWindow(object):
         self.label_insta_user.setText(_translate("MainWindow", "User:"))
         self.label_insta_pass.setText(_translate("MainWindow", "User:"))
         self.pushButton_GetPics.setText(_translate("MainWindow", "Get Pics"))
-        self.pushButton_Login.setText(_translate("MainWindow", "Login"))
-        self.label_step3.setText(_translate("MainWindow", "STEP3: UPLOAD PICS"))
-        self.pushButton_Upload.setText(_translate("MainWindow", "Upload"))
+        self.pushButton_LoginUpload.setText(_translate("MainWindow", "Login/Upload"))
+        self.pushButton_Save.setText(_translate("MainWindow", "Save"))
         self.label_Title.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:12pt; font-weight:600;\">FRONT PAGE SPORT NEWSPAPERS INSTAGRAM UPLOADER</span></p></body></html>"))
         self.label_Author.setText(_translate("MainWindow", "Powered by Flote (flote21@gmail.com)"))
         self.label_post.setText(_translate("MainWindow", "Post"))
-        self.label_hashtag.setText(_translate("MainWindow", "# hashtags"))
-
-        # load fix message in status bar
+        self.label_hashtag.setText(_translate("MainWindow", "# hashtags"))        # load fix message in status bar
         self.status_label = QtWidgets.QLabel()
         MainWindow.statusBar().addPermanentWidget(self.status_label, 100)
         self.status_label.setText(("Here is to show the evolution of every step..."))
 
+        # read data file lines
+        with open('data.txt') as fileData:
+            lines = fileData.readlines()
+        
+        fileLineCount = 0
+        for line in lines:
+            if fileLineCount == 0:
+                username = line[:-1]
+            elif fileLineCount == 1:
+                password = line[:-1]
+            elif fileLineCount == 2:
+                post_split = line.split('/')
+                post = post_split[0]
+                post = post[:-2]
+                break
+            fileLineCount += 1
+
+        with open('data.txt') as fileData:
+            content = fileData.read()        
+        starIndex = content.find("#")
+        hashtags = content[starIndex]
+        for i in range(starIndex+1,len(content)):
+            hashtags += content[i]
+            
         # Set default username & pass
-        self.line_insta_user.setText("portatest")
-        self.line_insta_pass.setText("kikazo")
+        self.line_insta_user.setText(username)
+        self.line_insta_pass.setText(password)
 
         # show default post and hashtags
-        date = time.strftime("%d/%m/%y")
-        post = "Portadas de hoy " + str(date)        
-        self.text_post.setPlainText(post)
-        hashtags = "#portadas #portad_as_ymas #diarioas#diariomarca\
-                    #diariosport #mundodeportivo #deporte#futbol\
-                    #laligasantander #uefachampionsleague #championsleague\
-                    #realmadridcf #zidane #sergioramos #benzema #modric\
-                    #cristianoronaldo #fcbarcelona #koeman #messi #pedri\
-                    #neymar #atleticodemadrid #simeone #marcosllorente\
-                    #oblak #jaofelix #luissuarez #hazard"
+        date = time.strftime("%d/%m/%y")        
+        self.text_post.setPlainText(post + str(date))
         self.text_hashtag.setPlainText(hashtags)
+
         # mouse click connect functions
         self.pushButton_GetPics.clicked.connect(self.getLinksAndPics)
-        self.pushButton_Login.clicked.connect(self.loginWebsites)
-        self.pushButton_Upload.clicked.connect(self.uploadPics)    
+        self.pushButton_LoginUpload.clicked.connect(self.loginUpload)
+        self.pushButton_Save.clicked.connect(self.SaveToFile)    
         
 
     def getLinksAndPics(self): 
@@ -270,7 +281,7 @@ class Ui_MainWindow(object):
 
         self.status_label.setText(("Pics downloaded successfully, go to next step"))
 
-    def loginWebsites(self): 
+    def loginUpload(self): 
     # Method used to login into the websited       
         # load studio creator webpage
         window_before, self.driver = studioCreatorLogin()                      
@@ -283,16 +294,25 @@ class Ui_MainWindow(object):
         # Wait 3sec until the old windows is loaded
         time.sleep(3)
         # Switch to the old window
-        self.driver.switch_to_window(window_before)
-        self.status_label.setText(("Login successfully, go to next step"))          
-
-    def uploadPics(self):         
-        studioCreatorUpload(self.driver, self.text_post, self.text_hashtag)
-        time.sleep(5)
+        self.driver.switch_to_window(window_before)        
+        time.sleep(3)
+        studioCreatorUpload(self.driver, self.text_post, self.text_hashtag)        
         #instagramLogout(self.driver, self.username)         
         self.driver.close()
-        self.status_label.setText(("Pics upload successfully!"))
+        self.status_label.setText(("Pics upload successfully!, to save current user&pass&post&#hastags press 'save' button..."))
 
+    def SaveToFile(self):
+        username = self.line_insta_user.text()
+        password = self.line_insta_pass.text()
+        post = self.text_post.toPlainText()
+        hashtags = self.text_hashtag.toPlainText()
+        fileData = open("data.txt","w+")
+        fileData.write(username + '\n')
+        fileData.write(password + '\n')
+        fileData.write(post + '\n')
+        fileData.write(hashtags + '\n')
+        fileData.close()
+        self.status_label.setText(("Data saved succesfully!"))
         
 if __name__ == "__main__":
     import sys
